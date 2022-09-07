4478b4c15654e40985aed691bb7d17c1b0290331
ryi01 <55128336+ryi01@users.noreply.github.com>
2022-09-07 20:13:29 +0900

    Nothing

--- Assets/MK/Script/PlayerGetItem.cs
+++ Assets/MK/Script/PlayerGetItem.cs
@@ -24,7 +24,7 @@ public class PlayerGetItem : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        item = GameObject.Find()
+        
     }
 
     // Update is called once per frame
