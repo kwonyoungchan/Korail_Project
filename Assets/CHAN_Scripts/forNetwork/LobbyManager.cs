a0aa721731915c6429cf19f7f706a7312d8a27df
kwonyoungchan <kyckyc0530@gmail.com>
2022-09-18 13:41:19 +0900

    add_lobby

--- Assets/CHAN_Scripts/forNetwork/LobbyManager.cs
+++ Assets/CHAN_Scripts/forNetwork/LobbyManager.cs
@@ -9,24 +9,29 @@ namespace Assets.Scripts
 {
     public class LobbyManager : MonoBehaviourPunCallbacks
     {
+        [Header("여기는 방생성 버튼관련 설정 모음")]
+        //  방생성
+        public Button btnCreate;
         //  방이름
         public InputField roomName;
         //비밀번호 InputField
-        public InputField InputPassword;
+        public InputField inputPassword;
         //  총인원
         public InputField totalNum;
-        //  방참가
-        public Button btnCreate;
+        // 생성 
+        public Button create;
+        int flag=1;
+        [Header("여기는 방입장 버튼관련 설정 모음")]
         //  방입장
         public Button btnJoin;
         //접속 Button
 
         //방의 정보들
-        Dictionary<string, RoomInfo> roomCache = new Dictionary<string, RoomInfo>();
+       // Dictionary<string, RoomInfo> roomCache = new Dictionary<string, RoomInfo>();
         //룸 리스트 content
-        public Transform trListContent;
+        //public Transform trListContent;
 
-        public GameObject[] mapThumbs;
+        //public GameObject[] mapThumbs;
         public void OnRoomNameChanged(string s)
         {
             //참가
@@ -45,7 +50,9 @@ namespace Assets.Scripts
             //방이름(InputField) 이 변경될 때 호출되는 함수 등록
             roomName.onValueChanged.AddListener(OnRoomNameChanged);
             totalNum.onValueChanged.AddListener(OnTotalPlayerChanged);
-            //CreateRoom();
+            // 시작부터 방생성 옵션들을 모두 끈다. 
+            TurnCreateBtn(false);
+
 
         }
         //방생성
@@ -62,15 +69,15 @@ namespace Assets.Scripts
             // 커스텀 정보를 셋팅
             ExitGames.Client.Photon.Hashtable hash = new ExitGames.Client.Photon.Hashtable(); ;
             hash["desc"] = "여긴 초보방" + Random.Range(1, 1000);
-            hash["map_id"] = Random.Range(0, mapThumbs.Length);
+           // hash["map_id"] = Random.Range(0, mapThumbs.Length);
             hash["room_name"] = roomName.text;
-            hash["password"] = InputPassword.text;
+            hash["password"] = inputPassword.text;
             roomOptions.CustomRoomProperties = hash;
             //print((string)hash["desc"]+ ", " + (float)hash[1]);
             //custom 정보를 공개하는 설정
             roomOptions.CustomRoomPropertiesForLobby = new string[] { "desc", "map_id", "room_name", "password" };
             // 방을 만든다.
-            PhotonNetwork.CreateRoom(roomName.text + InputPassword.text, roomOptions, TypedLobby.Default);
+            PhotonNetwork.CreateRoom(roomName.text + inputPassword.text, roomOptions, TypedLobby.Default);
         }
         //방입장
         //방 생성 완료
@@ -88,7 +95,7 @@ namespace Assets.Scripts
         }
         public void JoinRoom()
         {
-            PhotonNetwork.JoinRoom(roomName.text + InputPassword.text);
+            PhotonNetwork.JoinRoom(roomName.text + inputPassword.text);
         }
         public override void OnJoinedRoom()
         {
@@ -106,47 +113,47 @@ namespace Assets.Scripts
         {
             base.OnRoomListUpdate(roomList);
             //룸리스트 UI를 전체삭제
-            DeleteRoomListUI();
+            //DeleteRoomListUI();
             //룸리스트 정보를 업데이트
-            UpdateRoomCache(roomList);
+            //UpdateRoomCache(roomList);
             //룸리스트UI 전체 생성
             //CreateRoomListUI();
         }
-        void DeleteRoomListUI()
-        {
-            foreach (Transform tr in trListContent)
+        //void DeleteRoomListUI()
+        //{
+        //    foreach (Transform tr in trListContent)
 
-            {
-                Destroy(tr.gameObject);
-            }
-        }
-        void UpdateRoomCache(List<RoomInfo> roomList)
-        {
-            for (int i = 0; i < roomList.Count; i++)
-            {
-                //수정, 삭제
-                if (roomCache.ContainsKey(roomList[i].Name))
-                {
-                    //만약 해당 룸이 삭제된것이라면
-                    if (roomList[i].RemovedFromList)
-                    {
-                        //roomCache에서 해당 정보를 삭제
-                        roomCache.Remove(roomList[i].Name);
-                    }
-                    //그렇지 않다면
-                    else
-                    {
-                        //정보 수정
-                        roomCache[roomList[i].Name] = roomList[i];
-                    }
-                }
-                //추가 
-                else
-                {
-                    roomCache[roomList[i].Name] = roomList[i];
-                }
-            }
-        }
+        //    {
+        //        Destroy(tr.gameObject);
+        //    }
+        //}
+        //void UpdateRoomCache(List<RoomInfo> roomList)
+        //{
+        //    for (int i = 0; i < roomList.Count; i++)
+        //    {
+        //        //수정, 삭제
+        //        if (roomCache.ContainsKey(roomList[i].Name))
+        //        {
+        //            //만약 해당 룸이 삭제된것이라면
+        //            if (roomList[i].RemovedFromList)
+        //            {
+        //                //roomCache에서 해당 정보를 삭제
+        //                roomCache.Remove(roomList[i].Name);
+        //            }
+        //            //그렇지 않다면
+        //            else
+        //            {
+        //                //정보 수정
+        //                roomCache[roomList[i].Name] = roomList[i];
+        //            }
+        //        }
+        //        //추가 
+        //        else
+        //        {
+        //            roomCache[roomList[i].Name] = roomList[i];
+        //        }
+        //    }
+        //}
 
         public GameObject roomItemFac;
         //void CreateRoomListUI()
@@ -171,20 +178,40 @@ namespace Assets.Scripts
         //이전 썸네일 Id
         //초기에는 이전의 맵 id가 없으므로 -1
         int prevMap_id = -1;
-        void SetRoomName(string room, int map_id)
+        //void SetRoomName(string room, int map_id)
+        //{
+        //    //룸네일 설정
+        //    roomName.text = room;
+        //    // 이전 맵 썸네일을 모두 비활성화
+        //    // 만약에 이전 맵 썸네일이 활성화되어 있다면
+        //    //맵 썸네일 설정
+        //    if (prevMap_id > -1)
+        //    {
+        //        mapThumbs[prevMap_id].SetActive(false);
+        //    }
+        //    mapThumbs[map_id].SetActive(true);
+        //    //이전의 맵 id 저장
+        //    prevMap_id = map_id;
+        //}
+        //방생성 설정창 나오도록 하는 함수
+        public void OnClickedCreateRoom()
         {
-            //룸네일 설정
-            roomName.text = room;
-            // 이전 맵 썸네일을 모두 비활성화
-            // 만약에 이전 맵 썸네일이 활성화되어 있다면
-            //맵 썸네일 설정
-            if (prevMap_id > -1)
+            flag *= -1;
+            if (flag == -1)
             {
-                mapThumbs[prevMap_id].SetActive(false);
+                TurnCreateBtn(true);
             }
-            mapThumbs[map_id].SetActive(true);
-            //이전의 맵 id 저장
-            prevMap_id = map_id;
+            else { TurnCreateBtn(false); }
+            //방 생성 버튼을 누르면
+            //방이름, 비밀번호, 인원수 설정 UI, 그리고 생성 버튼이 나오도록한다. 
+            //다시 방생성 버튼을 누르면 해당 버튼이 없어지도록 만든다.
+        }
+        void TurnCreateBtn(bool B)
+        {
+            roomName.transform.gameObject.SetActive(B);
+            inputPassword.transform.gameObject.SetActive(B);
+            totalNum.transform.gameObject.SetActive(B);
+            create.transform.gameObject.SetActive(B);
         }
     }
 }
